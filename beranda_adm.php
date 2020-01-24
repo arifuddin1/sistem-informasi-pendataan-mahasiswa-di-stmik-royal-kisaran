82ae781e324dcd7d7b820cf2e63a8600a3965de3
arifuddin1 <60189586+arifuddin1@users.noreply.github.com>
2020-01-24 19:54:54 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+            <div class="alert alert-warning">
+			<marquee direction = "up" > <center>
+			Selamat Datang kembali<strong> <?=$_SESSION['nama']?></strong> </marquee></center>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <h3 class="panel-title"><center>Halaman Administrator Sistem Informasi Pendataan Mahasiswa STMIK Royal Kisaran</h3></center>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/royal.png"></img></p>
+                          <p class="label-title" align="center"><strong>Sistem Informasi Pendataan Mahasiswa STMIK Royal Kisaran</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Pendataan Mahasiswa STMIK Royal Kisaran.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data Mahasiswa, <br>
+                            dan juga pengelolaan laporan Pendataan Mahasiswa.<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data Mahasiswa.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
---
+++ footer.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+		<center><strong><font face = "courier new">
+            Copyright &COPY; <?= date('Y')?> M.Bayu Setiawan, M.Syarifuddin, Yuni Yustika Nst |
+            <a href="#">Dokumentasi</a>
+            <a href="#">Peta Situs</a>
+            <a href="#">Support</a></center></strong>
+        </div>
+    </div>
+</div>
